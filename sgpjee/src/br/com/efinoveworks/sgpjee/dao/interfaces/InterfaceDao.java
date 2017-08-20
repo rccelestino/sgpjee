b918136387a4be43a7cc350679cc852c16af15e7
Roberto <rcs.celestino@gmail.com>
2017-08-20 12:20:33 -0300

    Implementando alguns metodos classe ImplementacaoDao

--- sgpjee/src/br/com/efinoveworks/sgpjee/dao/interfaces/InterfaceDao.java
+++ sgpjee/src/br/com/efinoveworks/sgpjee/dao/interfaces/InterfaceDao.java
@@ -18,45 +18,45 @@ public interface InterfaceDao<T> extends Serializable {
 	void save(T obj) throws Exception;
 
 	void persist(T obj) throws Exception;
-	
+
 	void saveOrUpdate(T obj) throws Exception;
-	
+
 	void update(T obj) throws Exception;
-	
+
 	void delete(T obj) throws Exception;
-	
-	T merge (T obj) throws Exception;
-	
+
+	T merge(T obj) throws Exception;
+
 	List<T> findList(Class<T> objs) throws Exception;
-	
+
 	Object findById(Class<T> entidade, Long id) throws Exception;
-	
+
 	T findByPorId(Class<T> entidade, Long id) throws Exception;
-	
+
 	List<T> findListByQueryDinamica(String s) throws Exception;
-	
+
 	void executeUpdateQueryDinamica(String s) throws Exception;
-	
+
 	void executeUpdateSQLDinamica(String s) throws Exception;
-	
+
 	void clearSession() throws Exception;
-	
+
 	void evict(Object obj) throws Exception;
-		
+
 	Session getSession() throws Exception;
-	
+
 	List<?> getListSQLDinamica(String g) throws Exception;
-	
+
 	JdbcTemplate getJdbcTemplate();
-	
+
 	SimpleJdbcTemplate getSimpleJdbcTemplate();
-	
+
 	SimpleJdbcInsert getSimpleJdbcInsert();
-	
+
 	Long totalRegistro(String table) throws Exception;
-	
+
 	Query obterQuery(String query) throws Exception;
-	
+
 	List<T> findListByQueryDinamica(String query, int iniciaNoRegsitro, int maximoResultado) throws Exception;
-	
+
 }
