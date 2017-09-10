38f1d08c6b6cb3fba7c255e02d2275d821e30772
Roberto <rcs.celestino@gmail.com>
2017-09-10 10:23:19 -0300

    Criação Classe ViewScope do Spring [R].10-09-2017.

---
+++ sgpjee/src/br/com/efinoveworks/sgpjee/bean/geral/ViewScope.java
@@ -0,0 +1,73 @@
+package br.com.efinoveworks.sgpjee.bean.geral;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.web.context.request.FacesRequestAttributes;
+
+public class ViewScope implements Serializable, Scope {
+
+	private static final long serialVersionUID = 7489162231764207935L;
+
+	public static final String VIEW_SCOPE__CALLBACKS = "viewScope.callBacks";
+
+	@Override
+	public Object get(String name, ObjectFactory<?> objectFactory) {
+		Object instance = getViewMap().get(name);
+		if (instance == null) {
+			instance = objectFactory.getObject();
+			getViewMap().put(name, instance);
+		}
+		return instance;
+	}
+
+	@Override
+	public String getConversationId() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		FacesRequestAttributes facesRequestAttributes = new FacesRequestAttributes(facesContext);
+		return facesRequestAttributes.getSessionId() + " - " + facesContext.getViewRoot().getViewId();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void registerDestructionCallback(String name, Runnable runnable) {
+		Map<String, Runnable> callbacks = (Map<String, Runnable>) getViewMap().get(VIEW_SCOPE__CALLBACKS);
+
+		if (callbacks != null) {
+			callbacks.put(VIEW_SCOPE__CALLBACKS, runnable);
+
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object remove(String name) {
+		Object instance = getViewMap().remove(name);
+		if (instance != null) {
+			Map<String, Runnable> callBacks = (Map<String, Runnable>) getViewMap().get(VIEW_SCOPE__CALLBACKS);
+			if (callBacks != null) {
+				callBacks.remove(name);
+			}
+		}
+		return instance;
+	}
+
+	@Override
+	public Object resolveContextualObject(String name) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		FacesRequestAttributes facesRequestAttributes = new FacesRequestAttributes(facesContext);
+		return facesRequestAttributes.resolveReference(name);
+	}
+
+	private Map<String, Object> getViewMap() {
+		return FacesContext.getCurrentInstance() != null ? FacesContext.getCurrentInstance().getViewRoot().getViewMap()
+				: new HashMap<String, Object>();
+	}
+
+}
---
+++ sgpjee/src/br/com/efinoveworks/sgpjee/bean/geral/ViewScopeCallBackRegistrer.java
@@ -0,0 +1,49 @@
+package br.com.efinoveworks.sgpjee.bean.geral;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PostConstructViewMapEvent;
+import javax.faces.event.PreDestroyViewMapEvent;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.ViewMapListener;
+
+public class ViewScopeCallBackRegistrer implements Serializable, ViewMapListener {
+
+	private static final long serialVersionUID = -866261738553382290L;
+
+	@Override
+	public boolean isListenerForSource(Object source) {
+		return source instanceof UIViewRoot;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void processEvent(SystemEvent event) throws AbortProcessingException {
+
+		if (event instanceof PostConstructViewMapEvent) {
+			PostConstructViewMapEvent viewMapEvent = (PostConstructViewMapEvent) event;
+			UIViewRoot uiViewRoot = (UIViewRoot) viewMapEvent.getComponent();
+			uiViewRoot.getViewMap().put(ViewScope.VIEW_SCOPE__CALLBACKS, new HashMap<String, Runnable>());
+		} else if (event instanceof PreDestroyViewMapEvent) {
+
+			PreDestroyViewMapEvent viewMapEvent = (PreDestroyViewMapEvent) event;
+			UIViewRoot uiViewRoot = (UIViewRoot) viewMapEvent.getComponent();
+			Map<String, Runnable> callbacks = (Map<String, Runnable>) uiViewRoot.getViewMap()
+					.get(ViewScope.VIEW_SCOPE__CALLBACKS);
+
+			if (callbacks != null) {
+				for (Runnable c : callbacks.values()) {
+					c.run();
+				}
+				callbacks.clear();
+			}
+
+		}
+
+	}
+
+}
